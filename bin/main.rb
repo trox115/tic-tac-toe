a7f8c58d4bcd8f43a89ee77fa12c0f9f05813eeb
trox115 <tofernandes67@gmail.com>
2019-07-15 21:15:19 +0100

    correct format

--- bin/main.rb
+++ bin/main.rb
@@ -1,83 +1,79 @@
 require '../lib/library.rb'
 class Game
-    attr_accessor :players, :board, :symbols, :turn_toplay
-    def initialize
-        @board = Board.new
-        @players = []
-        
-        @playermoves = [[],[]]
-        @turn_toplay = 0 #it changes from 0 to 1 depending on the turn
-        @symbols = ["X", "O"]
-        
-    end
+  attr_accessor :players, :board, :symbols, :turn_toplay
+  def initialize
+    @board = Board.new
+    @players = []
 
-    def welcome
-        puts "------------------------------------------------------------"
-        puts "Welcome at this tic-tac-toe game, built by António Fernandes And Carlos del Real"
-        puts "------------------------------------------------------------/n"
-        puts "what is the name of player 1 ?"
-        player1 = gets.chomp
-        puts "what is the name of player 2 ?"
-        player2 = gets.chomp
-        puts "Sorting the first turn ..."
-        @players = [player1, player2]
-        if @players.sample == player2
-            @players.reverse!
-        end
-        puts "Hello #{@players[0]} you go X and #{@players[1]} plays with O"
+    @playermoves = [[], []]
+    @turn_toplay = 0 # it changes from 0 to 1 depending on the turn
+    @symbols = %w[X O]
+  end
+
+  def welcome
+    puts '------------------------------------------------------------'
+    puts 'Welcome at this tic-tac-toe game, built by António Fernandes And Carlos del Real'
+    puts '------------------------------------------------------------/n'
+    puts 'what is the name of player 1 ?'
+    player1 = gets.chomp
+    puts 'what is the name of player 2 ?'
+    player2 = gets.chomp
+    puts 'Sorting the first turn ...'
+    @players = [player1, player2]
+    @players.reverse! if @players.sample == player2
+    puts "Hello #{@players[0]} you go X and #{@players[1]} plays with O"
+  end
+
+  def play
+    @board.print_board
+
+    loop do
+      puts "Turn to: #{@players[@turn_toplay]}, insert a digit"
+      played = gets.chomp
+      valid = @board.choose(played.to_i, @symbols[@turn_toplay])
+
+      next unless valid
+
+      @playermoves[@turn_toplay].push(played.to_i)
+      if @board.checking(@playermoves[@turn_toplay]) == true
+        puts ''
+        puts ''
+        puts "The winner is #{@players[@turn_toplay]}"
+        puts ''
+        puts ''
+        break
+
+      end
+      switch_turn
     end
-    def play
-        @board.print_board
-        
-        while true
-            puts "Turn to: #{@players[@turn_toplay]}, insert a digit"
-            played = gets.chomp
-            valid = @board.choose(played.to_i, @symbols[@turn_toplay])
-            
-            if valid
-                @playermoves[@turn_toplay].push(played.to_i)
-                if @board.checking(@playermoves[@turn_toplay])==true
-                    puts ""
-                    puts ""
-                    puts "The winner is #{@players[@turn_toplay]}"
-                    puts ""
-                    puts ""
-                    break
-                   
-                end             
-                switch_turn
-            end
-        end
-    end
-    def switch_turn
-        if @turn_toplay == 0
-            @turn_toplay = 1
-        else
-            @turn_toplay = 0
-        end
-    end
-    def turn(player)
+  end
+
+  def switch_turn
+    @turn_toplay = if @turn_toplay.zero?
+                     1
+                   else
+                     0
+                   end
+  end
+
+  def turn(_player)
+    turn_valid = false
+    until turn_valid
+      begin
+        puts "#{@players[@turn_toplay]} it's your turn, insert a digit"
+        number = gets.chomp
+        turn_valid = true if number.integer?
+      rescue StandardError
         turn_valid = false
-        while !turn_valid
-            begin
-                puts "#{@players[@turn_toplay]} it's your turn, insert a digit"
-                number = gets.chomp
-                if number.integer?
-                    turn_valid = true
-                  
-                end
-            rescue
-                turn_valid = false
-                puts "Insert a valid Integer"
-            end
-        end
-        @board.choice(number)
+        puts 'Insert a valid Integer'
+      end
     end
+    @board.choice(number)
+  end
 end
 
-
-while true
-    game = Game.new
-game.welcome
-game.play
+loop do
+  game = Game.new
+  game.welcome
+  game.play
 end
--- lib/library.rb
+++ lib/library.rb
@@ -1,49 +1,46 @@
 class Board
-    attr_accessor :board, :board_numbers
-    def initialize
-        @board_numbers = [[1,2,3], [4,5,6], [7,8,9]]
-        @board = [[1,2,3], [4,5,6], [7,8,9]]
-        @winning = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[3,6,9],[1,4,7],[2,5,8]]
+  attr_accessor :board, :board_numbers
+  def initialize
+    @board_numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @winning = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [3, 6, 9], [1, 4, 7], [2, 5, 8]]
+  end
+
+  def print_board
+    @board.each do |x|
+      x.each do |y|
+        print "| #{y} |"
+      end
+      puts ''
     end
-     
-    def print_board
-        @board.each do |x|
-            x.each do |y|
-                print "| #{y} |"
-            end
-            puts ""
+  end
+
+  def choose(number, symbol = 'X')
+    (0...@board_numbers.length).each do |i|
+      (0...@board_numbers[i].length).each do |j|
+        if @board_numbers[i][j] == number
+          if @board[i][j].class == Integer
+            @board[i][j] = symbol
+            print_board
+            return true
+          else
+            puts 'Already Chosen'
+            print_board
+            return false
+          end
         end
+      end
     end
+  end
 
-    def choose(number, symbol="X")
-        for i in 0...@board_numbers.length
-            for j in 0...@board_numbers[i].length
-                if @board_numbers[i][j] == number
-                    if @board[i][j].class == Integer
-                        @board[i][j] = symbol
-                        self.print_board
-                        return true
-                    else
-                        puts "Already Chosen"
-                        self.print_board
-                        return false
-                    end
-                end
-            end
-        end
+  def checking(arr)
+    @winning.each do |x|
+      return true if contains_all?(arr, x)
     end
-    def checking(ar)
-        @winning.each do |x|
-            if contains_all?(ar,x)
-                return true
-            end
-        end
-        return false
-    end
+    false
+  end
 
-
-    def contains_all?(a,b)
-        b.uniq.all? { |x| a.count(x) >= b.count(x) }
-    end
-
+  def contains_all?(aaa, bbb)
+    bbb.uniq.all? { |x| aaa.count(x) >= bbb.count(x) }
+  end
 end
