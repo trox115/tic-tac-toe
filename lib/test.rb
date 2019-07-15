1e0469e7365f01a1fd92f261f51a27002b20367d
Carlos Del Real <carloshdelreal@gmail.com>
2019-07-15 12:14:20 -0500

    board class created, print board method finished and choose method

---
+++ lib/test.rb
@@ -0,0 +1,15 @@
+require "./library.rb"
+
+board = Board.new
+
+
+board.choose(1)
+board.choose(2)
+board.choose(3)
+board.choose(4)
+board.choose(5)
+board.choose(6)
+board.choose(7)
+board.choose(8)
+board.choose(9)
+board.choose(9)
